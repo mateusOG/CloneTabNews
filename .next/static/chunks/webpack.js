3ebdd80ee681d15a83d5fe54d25654e7ca531e5e
Mateus Oliveira Gil <67986632+mateusOG@users.noreply.github.com>
2024-04-11 00:00:28 +0000

    adicionado .env e database.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2f59a7654c212201"; }
+/******/ 		__webpack_require__.h = function() { return "d301c6048ac4312f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
